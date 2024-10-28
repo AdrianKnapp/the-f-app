9c174e31aec86e55b0aeb8edff463e9f3f637d27
AdrianKnapp <adrknapp@gmail.com>
2024-10-28 19:59:21 -0300

    feat(app): added first component

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,31 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [Colors.deepPurple, Colors.deepPurpleAccent],
-          ),
+      body: GradientContainer(),
+    ),
+  ));
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [Colors.deepPurple, Colors.deepPurpleAccent],
         ),
-        child: const Center(
-          child: Text(
-            'Hello World!',
-            style: TextStyle(
-              fontSize: 28,
-              color: Colors.white,
-            ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello World!',
+          style: TextStyle(
+            fontSize: 28,
+            color: Colors.white,
           ),
         ),
       ),
-    ),
-  ));
+    );
+  }
 }
